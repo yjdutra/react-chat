a05979d7022676d02644822ec4b489648df9780b
yuri <yjdutra@gmail.com>
2022-09-06 08:47:08 -0300

    [back/db]: firebase coinfig completed

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Sidebar() {
+  return <div>SideBar</div>;
+}
+
+export default Sidebar;
---
+++ src/components/SidebarChats/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function SidebarChats() {
+  return <div />;
+}
+
+export default SidebarChats;
